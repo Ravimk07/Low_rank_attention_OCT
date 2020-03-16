12449f96f176248a3e8340bc266899d9aad305a7
moucheng2017 <rmapmxu@ucl.ac.uk>
2020-03-16 22:54:17 +0000

    new data set set up duke-oct

--- Run_files/oct_b_3.sh
+++
@@ -1,7 +0,0 @@
-#$ -l tmem=7G
-#$ -l gpu=true
-#$ -S /bin/bash
-#$ -j y
-#$ -wd /cluster/project0/CityScapes/projects_codes/MICCAI_2020_OCT/Experiments
-
-~/anaconda3/bin/python baseline_relaynet_no_aug.py
--- Run_files/oct_o_8.sh
+++
@@ -1,7 +0,0 @@
-#$ -l tmem=8G
-#$ -l gpu=true
-#$ -S /bin/bash
-#$ -j y
-#$ -wd /cluster/project0/CityScapes/projects_codes/MICCAI_2020_OCT/Experiments
-
-~/anaconda3/bin/python ours_segnet_skip_no_aug.py
